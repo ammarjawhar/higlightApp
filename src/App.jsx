817e304e5f38967d4989728893ede35f8bfeb63b
ammargoher94@mail.com <ammargoher94@mail.com>
2025-08-10 15:47:57 +0300

    editting higlight component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 /* import Highlightinp from './Components/Highlightinp';*/
-import Highlightinp from 'input-highlight-react2';
-
+import Highlightinp from './components/Highlightinp';
 
 const App = () => {
   // you can pass any pattern as aprop to the higlight component
   const pattern = /(@\w+)|(#\w+)|(https?:\/\/[^\s]+)|({{[^}]+}})/g;
 
   return (
-    <div>
+    <div className="flex  justify-center items-center h-screen w-full">
       <Highlightinp pattern={pattern} />
     </div>
   );
